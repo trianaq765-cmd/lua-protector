848c55a96d3a67da3a1eab99f9d1362431b80617
trianaq765-cmd <trianaq765@gmail.com>
2025-12-28 15:43:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 // 🔧 CONFIGURATION
 // ============================================
 const CONFIG = {
-    LOADER_SCRIPT_URL: "https://pastebin.com/raw/u4kzwdEX",
+    LOADER_SCRIPT_URL: "https://pastebin.com/raw/WLqQXQUY",
     WORKINK_API: "https://work.ink/_api/v2/token/isValid/",
     
     DB_FILE: './database.json',
